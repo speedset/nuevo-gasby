c737f2ab37e90aa24914fe699ae096495a191c5e
adrian manchas <garciadaniloadrian@gmail.com>
2024-01-26 17:56:46 +0000

    Agregar imagenes con complementos de gatsby

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,15 +1,15 @@
 // Step 1: Import React
 import * as React from "react";
-import { Link } from "gatsby";
+import Layout from "../components/layout";
+
 // Step 2: Define your component
 const AboutPage = () => {
   return (
-    <main>
-      <h1>About Me</h1>
+    <Layout pageTitle={'About Me'}>
       <p>
         Hi there! I'm the proud creator of this site, which I built with Gatsby.
-      </p>
-    </main>
+      </p>      
+    </Layout>
   );
 };
 export const Head = () => (
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,17 @@
 // Step 1: Import React
 import * as React from 'react'
-import { Link } from 'gatsby'
-
+import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 // Step 2: Define your component
 const IndexPage = () => {
   return (
-    <main>
-      <h1>Welcome to my Gatsby site!</h1>
-      <Link to='/about'>About</Link>
+    <Layout pageTitle={'Home Page'}>
       <p>I'm making this by following the Gatsby Tutorial.</p>
-    </main>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="../images/perro-marron.webp"
+      />
+    </Layout>
   )
 }
 
